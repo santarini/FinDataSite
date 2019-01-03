798360998a9529c784b67c59f92e542e2f5cb471
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-03 00:59:07 -1000

    Update calendarScheduler.py

--- calendarScheduler.py
+++ calendarScheduler.py
@@ -106,3 +106,19 @@ for x in range(0,8):
 
 print(marketHolidays)
 
+
+##Easter
+year = 2019
+a = year % 19
+b = year >> 2
+c = b // 25 + 1
+d = (c * 3) >> 2
+e = ((a * 19) - ((c * 8 + 5) // 25) + d + 15) % 30
+e += (29578 - a - e * 32) >> 10
+e -= ((year % 7) + b - d + e + 2) % 7
+d = e >> 5
+day = e - d * 31
+month = d + 3
+print(str(year) + "-" + str(month) + "-" + str(day))
+
+
