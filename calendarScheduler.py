cc22c06b37f36e69cefba2e2894c5ce23c25703a
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-02 17:45:25 -1000

    Update calendarScheduler.py

--- calendarScheduler.py
+++ calendarScheduler.py
@@ -12,4 +12,59 @@
 ##print(common_timezones)
 
 
-months = ['January','February','March','April', 'May','June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+import datetime
+
+#get year
+
+now = datetime.datetime.now()
+
+#check if leap year
+if not(int(now.year) % 4 == 0):
+    print(str(now.year) + " Common Year")
+    febDays = 28
+elif not(int(now.year) % 100 == 0):
+    print(str(now.year) + " Leap Year")
+    febDays = 29
+elif not(int(now.year) % 400 == 0):
+    print(str(now.year) + " Common Year")
+    febDays = 28
+else:
+    print(str(now.year) + " Common Year")
+    febDays = 28
+
+######
+
+months = [['January', 31],
+          ['February', febDays],
+          ['March', 31],
+          ['April', 30],
+          ['May', 31],
+          ['June', 30],
+          ['July', 31],
+          ['August', 31],
+          ['September',30],
+          ['October', 31],
+          ['November', 30],
+          ['December', 31]
+          ]
+
+i = 0
+while i < 12:
+    j = 1
+    while j <= months[i][1]:
+        print(datetime.date(now.year, i+1, j))
+        j += 1
+    i +=1
+
+
+##datetime_object = datetime.date(now.year, 12, 21)
+##print(datetime_object)
+##
+##holidays = []
+##
+##months = ['January','February','March','April', 'May','June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+#https://www.wikiwand.com/en/Federal_holidays_in_the_United_States#/List_of_federal_holidays
+#See floating Days
+
