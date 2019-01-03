a4be2e418c02002c84fe22b9b4b4178c7dd85c12
Makoa Santarini <santarinimakoa@gmail.com>
2019-01-02 22:10:03 -1000

    Created function for floating date holidays

--- calendarScheduler.py
+++ calendarScheduler.py
@@ -11,13 +11,15 @@
 ##print(datetime.now(tz))
 ##print(common_timezones)
 
-
+marketHolidays = []
 
 import datetime
 
 #get year
 
 now = datetime.datetime.now()
+
+MLKDay = datetime.date(now.year, 1, 1)
 
 #check if leap year
 if not(int(now.year) % 4 == 0):
@@ -35,39 +37,91 @@ else:
 
 ######
 
-months = [['January', 31],
-          ['February', febDays],
-          ['March', 31],
-          ['April', 30],
-          ['May', 31],
-          ['June', 30],
-          ['July', 31],
-          ['August', 31],
-          ['September',30],
-          ['October', 31],
-          ['November', 30],
-          ['December', 31]
-          ]
+daysInWeek = ["Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+        ]
 
-days = ["Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday"
-            ]
+monthsInYear = {'January': [31, 1],
+                'February': [febDays, 2],
+                'March': [31, 3],
+                'April': [30, 4],
+                'May': [31, 5],
+                'June': [30, 6],
+                'July': [31, 7],
+                'August': [31, 8],
+                'September': [30, 9],
+                'October': [31, 10],
+                'November': [30, 11],
+                'December': [31, 12]
+                }
 
-i = 0
-while i < 12:
-    j = 1
-    while j <= months[i][1]:
-        fullDate = datetime.date(now.year, i+1, j)
-        weekdayInt = datetime.date(now.year, i+1, j).weekday()
-        if weekdayInt < 5:
-            print(str(fullDate) + " " + days[weekdayInt])
-        j += 1
-    i +=1
+##holidaysVariables = [["New Year's Day", NewYears],
+##                     ["Martin Luther King, Jr. Day", MLKDay]
+##                     ]
+
+holidaysData = [["New Year's Day", "Fixed", datetime.date(now.year, 1, 1), "Closed"],
+                ["Martin Luther King, Jr. Day", "Variable", 'January', 'Monday', 3, "Closed"],
+                ["President's Day", "Variable", 'February', 'Monday', 3, "Closed"],
+                ]
+
+def floatingHoliday(holidayMonth, holidayWeekday, holidayWeekdayCount):
+    i = 1
+    for j in range(1,monthsInYear[holidayMonth][0]):
+        fullDate = datetime.date(now.year, monthsInYear[holidayMonth][1], j)
+        weekdayInt = datetime.date(now.year, monthsInYear[holidayMonth][1], j).weekday()
+        if weekdayInt == 0:
+            if i == holidayWeekdayCount:
+                marketHolidays.append(fullDate)
+            i+=1
+
+
+for x in range(0,3):
+    if holidaysData[x][1] == "Fixed":
+        marketHolidays.append(holidaysData[x][2])
+    else:
+        floatingHoliday(holidaysData[x][2],holidaysData[x][3],holidaysData[x][4])
+
+print(marketHolidays)
+
+
+
+##15 January 2018 
+##Martin Luther King, Jr. Day 
+##Closed 
+##19 February 2018 
+##President's Day 
+##Closed 
+##30 March 2018 
+##Good Friday 
+##Closed
+##28 May 2018 
+##Memorial Day 
+##Closed 
+##3 July, 2018 
+##Early Market Close 
+##1:00 PM EST 
+##4 July 2018 
+##Independence Day (Observed) 
+##Closed 
+##3 September 2018 
+##Labor Day 
+##Closed 
+##22 November 2018 
+##Thanksgiving Day	Closed 
+##23 November 2018 
+##Early Market Close 
+##1:00 p.m. EST
+##24 December 2018 
+##Christmas Eve -- Early Market Close 
+##1:00 p.m. EST
+##25 December 2018 
+##Christmas Day (Observed) 
+##Closed
 
 
 ##datetime_object = datetime.date(now.year, 12, 21)
