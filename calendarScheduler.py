2f58b71fd01fd984171b51262a8b684c1171d30d
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-02 17:07:54 -1000

    Create calendarScheduler.py

---
+++ calendarScheduler.py
@@ -0,0 +1,15 @@
+#Get market closed dates
+#Get market hours of operations
+#For market hours of operations schedule executeion
+
+##from datetime import datetime
+##from pytz import timezone, common_timezones
+##
+##
+##tz = timezone('EST')
+##tz = timezone('US/Hawaii')
+##print(datetime.now(tz))
+##print(common_timezones)
+
+
+months = ['January','February','March','April', 'May','June', 'July', 'August', 'September', 'October', 'November', 'December']
